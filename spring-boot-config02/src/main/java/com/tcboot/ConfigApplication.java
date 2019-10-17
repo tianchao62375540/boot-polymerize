47253b7ce43b459166a6b5ec0897fd6ae25265f5
tianchao <455433520@qq.com>
2019-10-17 14:01:34 +0800

    banner

---
+++ spring-boot-config02/src/main/java/com/tcboot/ConfigApplication.java
@@ -0,0 +1,19 @@
+package com.tcboot;
+
+/**
+ * Description:
+ * <p>
+ * ModelName:【】模块
+ *
+ * @author: TianChao
+ * Create at:  2019/10/12 17:22
+ * Company: 沈阳艾尔时代科技发展有限公司
+ * Copyright: (c)2018 AIR Times Inc. All rights reserved.
+ * @version: 1.0
+ */
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
